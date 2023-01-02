794ccc505e1e6c61892d970c0294a30a251d1a78
Mazen <mazenghaleb@outlook.com>
2023-01-02 22:46:14 +0200

    Fixed small mistake in tracking

--- app/src/main/java/com/example/wagba/view/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/wagba/view/Fragments/HomeFragment.java
@@ -73,7 +73,7 @@ public class HomeFragment extends BaseFragment {
 
                 if (dataSnapshot.hasChild("Orders")) {
                     for (DataSnapshot order : dataSnapshot.child("Orders").getChildren()){
-                        if (order.child("orderStatus").getValue().equals("Verifying")){
+                        if (!order.child("orderStatus").getValue().equals("Delivered") || !order.child("orderStatus").getValue().equals("Denied")){
                             currentOrderRestaurant.setText(order.child("restaurant").getValue().toString());
                             currentOrderStatus.setText(order.child("orderStatus").getValue().toString());
                             currentOrderETA.setText("60 mins");
